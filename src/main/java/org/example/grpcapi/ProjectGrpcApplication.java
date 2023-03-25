1048e202ddc980abff72851318fe35295ff28095
Nitish Gupta <nitishgupta1101@gmail.com>
2023-03-26 02:25:17 +0530

    Initial Commit

---
+++ src/main/java/org/example/grpcapi/ProjectGrpcApplication.java
@@ -0,0 +1,13 @@
+package org.example.grpcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGrpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/grpcapi/ProjectGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grpcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
